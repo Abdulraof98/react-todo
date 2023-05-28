aa7c6f57126a0483cbfedca24c8d114715225f10
Abdul Raof Froutan <abdulraof.froutan@gmail.com>
2023-05-28 17:04:10 +0530

    second cmt

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Raof = () => {
   return (
-    <div>
+    <div>  
+    jjkkjiiiiasds
       <div style={{width: '300px', height: '300px', backgroundColor: 'gray', fontSize: '50px', borderRadius: '20px 20px auto auto'}}></div>
     </div>
   )
